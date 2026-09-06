9f99cfef965e2bad2d9eff4de59c3343e17bd87f
Anirudh Gupta <123016836+TheRealAniG@users.noreply.github.com>
2026-09-06 15:46:53 -0700

    initial code

---
+++ lab1/lab1_blinker.sv
@@ -0,0 +1,30 @@
+module blink(
+	input   logic int_osc,
+	input   logic reset,
+	output  logic led[2:0] 
+);
+
+	logic [24:0] counter;
+   
+	// Counter
+    always_ff @(posedge int_osc) begin
+		if(reset == 0) begin
+			counter <= 0;
+		end
+		if(counter == 25'd19_999_999) begin
+			counter <= 0;
+			led[2] <= 1'b1;
+		end
+		else begin
+			counter <= counter + 1;
+			led[2] <= 1'b0;
+		end
+	end
+endmodule
+   
+   
+   
+   
+   
+   
+   
