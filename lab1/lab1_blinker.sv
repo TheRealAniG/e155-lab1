79eaf436911f5f48e72f48422551f294f99f82bf
Anirudh Gupta <123016836+TheRealAniG@users.noreply.github.com>
2026-09-06 16:14:56 -0700

    update

--- lab1/lab1_blinker.sv
+++ lab1/lab1_blinker.sv
@@ -1,7 +1,12 @@
+//Anirudh Gupta
+//anirgupta@g.hmc.edu
+//9/6/2026
+//This module blinks an led at 2.4 Hz
+
 module blink(
 	input   logic int_osc,
 	input   logic reset,
-	output  logic led[2:0] 
+	output  logic [2:0] led
 );
 
 	logic [24:0] counter;
@@ -11,7 +16,8 @@ module blink(
 		if(reset == 0) begin
 			counter <= 0;
 		end
-		if(counter == 25'd19_999_999) begin
+		//48 Mhz clock, so for 2.4Hz blink every 20 million cycles
+		if(counter == 24'd19_999_999) begin
 			counter <= 0;
 			led[2] <= 1'b1;
 		end
--- lab1/lab1_top.sv
+++ lab1/lab1_top.sv
@@ -1,22 +1,28 @@
+//Anirudh Gupta
+//anirgupta@g.hmc.edu
+//9/6/2026
+//Top module
+//Also controls LEDs based on switch inputs
+
 module top(
-     input   logic reset,
-     output  logic led
+	input   logic [3:0] s,
+	output  logic [2:0] led,
+	output  logic [6:0] seg
 );
 
-   logic int_osc;
-   logic [24:0] counter;
-
-   // Internal high-speed oscillator
-   HSOSC #(.CLKHF_DIV(2'b01))
-         hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
-
-   // Counter
-   always_ff @(posedge int_osc) begin
-     if(reset == 0)  counter <= 0;
-     else            counter <= counter + 1;
-   end
-
-   // Assign LED output
-   assign led = counter[24];
+	logic int_osc;
+	logic reset;
+   
+   //Clock generation
+	HSOSC #(.CLKHF_DIV(2'b01))
+		hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
+		 
+	//modules
+	blink blink(.int_osc(int_osc), .reset(reset), .led(led));
+	segment segment(.s(s), .seg(seg))
+	
+	//combinational logic for LEDs
+	assign led[0] = s[0] ^ s[1];
+	assign led[1] = s[2] & s[3];
 
 endmodule
